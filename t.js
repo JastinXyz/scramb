4a3839f03fb32818c562d82abd19f3f0cf7aacf0
JastinXyz <jastinlinggar@gmail.com>
2022-01-15 17:26:22 +0700

    v3.0.0

--- t.js
+++
@@ -1,4 +0,0 @@
-const s = require('./index')
-var e = s.rps()
-
-console.log(e)
--- t2.js
+++
@@ -1,9 +0,0 @@
-function validateEmail(email) {
-  var re = /\S+@\S+\.\S+/;
-  const res = {
-    result: re.test(email),
-  };
-  return res;
-}
-
-console.log(validateEmail("am"));
