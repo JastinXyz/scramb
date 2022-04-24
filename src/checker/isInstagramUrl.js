ed5c7110f7f5af1f1aa9bc7488ab51ee453c35c9
JastinXyz <73673322+JastinXyz@users.noreply.github.com>
2022-04-24 11:03:58 +0700

    benerin sedikit

--- src/checker/isInstagramUrl.js
+++ src/checker/isInstagramUrl.js
@@ -2,7 +2,7 @@ const isInstagramURL = (url) => {
     if (!url) {
         throw new Error("[ SCRAMB ERR ] value required.");
       } else {
-        var ig = new RegExp(/https:\/\/www.instagram.com\/(p|reel|tv)/, "gi")
+        var ig = new RegExp(/https:\/\/(www.|)instagram.com\/(p|reel|tv|)/, "gi")
         const res = {
           result: ig.test(url)
         };
